a354277e1fbd96cf2c58d6140d16f1d1c9025f30
Greg Wiley <gwiley@cj.com>
2018-09-01 15:27:07 -0700

    better wheel calcs

--- src/View/Wheel.js
+++ src/View/Wheel.js
@@ -21,31 +21,26 @@ const luminance = (hex, lum) => {
 	return rgb;
 };
 
-const radiusFactor = .077;
+const generatePoints = (count, dim, colors) => {
 
-const point = (count, dim) => {
-    const innerDim = dim - dim * radiusFactor * 2.1
+    const innerDim = dim * .81;
     const theta = (2 * Math.PI) / count;
-    return i => {
+    const radius = (Math.PI * innerDim * .9) / (count * 2)
+    const cidx = idx => idx+"_";
+    const combine = (m,{index,color}) => ({...m, [cidx(index)]:color});
+    const colors_ = R.reduce(combine, {}, colors);
+    return R.map(i => {
         const x = Math.cos(i * theta) * (innerDim/2) + (dim/2);
         const y = Math.sin(i * theta) * (innerDim/2) + (dim/2);
-      return  ({x,y});
-    };
-
+        const color = colors_[cidx(i)] || "off";
+        return {x,y,radius,color}
+    }, R.range(0, count));
 };
 
-const generatePoints = (colors, dim) => {
-
-    const coordOf = point(colors.length, dim)
-    const coords = R.map(coordOf, R.range(0,colors.length));
-    return R.zipWith ((color,coord) => ({...coord, color}), colors, coords);
-
-
-};
-
-const Dot = ({x,y,r = 30,color}) =>  {
+const Dot = ({x, y, radius, color}) =>  {
     const colorOutside = luminance(color,-.5);
     const gid = x+"_"+y
+    const fill = (color === "off") ? "#666" : "url(#" + gid + ")";
     return (
         <React.Fragment>
         <defs>
@@ -55,29 +50,32 @@ const Dot = ({x,y,r = 30,color}) =>  {
             </radialGradient>
 
         </defs>
-        <circle cx={x} cy={y} r={r} fill={"url(#" + gid + ")"} stroke="#cccccc" filter="url(#glow)"/>
+        <circle cx={x} cy={y} r={radius} fill={fill} stroke="#cccccc" filter="url(#glow)"/>
         </React.Fragment>
     );
 
 };
 
-const Wheel = ( {dim = 300, colors = ["#000000", "#CC0000", "#0000DD", "#00EE00", "#FF0000", "#000000", "#CC0000", "#0000DD", "#00EE00", "#FF0000", "#000000", "#CC0000", "#0000DD", "#00EE00", "#FF0000", "#FF2299"]}) =>
-<div>
-    <svg width={dim} height={dim} >
-        <defs>
-            <filter id="glow">
-                <feGaussianBlur stdDeviation="2.5" result="coloredBlur"/>
-                <feMerge>
-                    <feMergeNode in="coloredBlur"/>
-                    <feMergeNode in="SourceGraphic"/>
-                </feMerge>
-            </filter>
-        </defs>
-       {R.map(p => <Dot key ={p.x + "" + p.y} x={p.x} y={p.y} color={p.color} r={radiusFactor*dim} />, generatePoints(colors, dim))}
+const Wheel = ( {dim = 300, count = 16, colors = [{index: 3, color: "#FF3344"}] }) => {
+    const points = generatePoints(count, dim, colors);
+    return (
+        <div>
+            <svg width={dim} height={dim} >
+                <defs>
+                    <filter id="glow">
+                        <feGaussianBlur stdDeviation="2.5" result="coloredBlur"/>
+                        <feMerge>
+                            <feMergeNode in="coloredBlur"/>
+                            <feMergeNode in="SourceGraphic"/>
+                        </feMerge>
+                    </filter>
+                </defs>
+            {R.map(p => <Dot key ={p.x + "_" + p.y} x={p.x} y={p.y} color={p.color} radius={p.radius} />, points)}
 
-    </svg>
-</div>
-
+            </svg>
+        </div>
+    );
+};
 
 export {Wheel};
 

ebe44c7a2629d5cc5ef75ee02d0e943f296baf4e
Greg Wiley <gwiley@cj.com>
2018-09-01 21:24:45 -0700

    checkpoint

--- operations/cd-publish.sh
+++ operations/cd-publish.sh
@@ -2,15 +2,18 @@
 
 set -e
 
+orign='iot-demo-frontend-websiteorigin-q7qtqoasc02j'
+
+cd "$(dirname "${0}")"
+
 aws s3 cp \
     --recursive \
     --cache-control 'max-age=31536000' \
     --exclude index.html \
     ./build \
-    s3://operations-area-frontend-websiteorigin-1rtgo36nz66i0/
+    "s3://${origin}/"
 
 aws s3 cp \
     --cache-control 'max-age=900,s-maxage=30' \
     ./build/index.html \
-    s3://operations-area-frontend-websiteorigin-1rtgo36nz66i0/
-
+    "s3://${origin}/"

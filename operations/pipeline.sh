d1113cd03047f45c6f977ce3c7e3d92cc1f911af
Greg Wiley <gwiley@cj.com>
2018-09-01 22:36:05 -0700

    pipeline

--- operations/pipeline.sh
+++ operations/pipeline.sh
@@ -1,13 +1,21 @@
 #! /usr/bin/env bash
 
-cd $(dirname ${0})
+cd "$(dirname "${0}")"
 
 op=update
 if [ "$1" == "create" ]; then op=create; shift; fi
 
-# assume dev environment during rapid dev phase, switch to assume
-# production env before completing - gjw 2018-05-24
-aws cloudformation update-stack \
-    --stack-name operations-pipeline \
-    --template-body file://cf-pipeline.yaml \
-    --capabilities CAPABILITY_IAM
+ptoken="notreallyatoken"
+
+if [ "$1" != "" ]; then
+    prepo="ParameterKey=GithubToken,ParameterValue=${1}"
+    shift
+else
+    prepo="ParameterKey=GithubToken,UsePreviousValue=true"
+fi
+
+aws cloudformation "${op}-stack" \
+    --stack-name iot-demo-pipeline \
+    --template-body file://pipeline.yaml \
+    --capabilities CAPABILITY_IAM \
+    --parameters "$prepo"

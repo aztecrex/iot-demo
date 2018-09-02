80024bfa5362bcd5eef89e9b41a14145a8fcd5ca
Greg Wiley <gwiley@cj.com>
2018-09-02 12:26:30 -0700

    style

--- src/View/App.js
+++ src/View/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import './App.css';
+
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
+
 import { ConnectedNavBar } from './NavBar';
 import { ConnectedLogin } from './Login';
+import { ConnectedWheel } from './Wheel';
+import { ConnectedMatrix } from './Matrix';
+
+import './App.css';
+
+const theme1 = createMuiTheme({
+    palette: {
+        primary: {
+            // light: will be calculated from palette.primary.main,
+            main: '#ff4400',
+            // dark: will be calculated from palette.primary.main,
+            // contrastText: will be calculated to contrast with palette.primary.main
+          },
+    },
+});
 
 const App = () => (
-    <div>
+    <MuiThemeProvider theme={theme1}>
         <ConnectedLogin />
         <ConnectedNavBar />
-    </div>
+        <ConnectedWheel dim="250" device="colorwheel_3" />
+        <ConnectedMatrix dim = "250" device="matrix_0" />
+    </MuiThemeProvider>
 );
 
 export {App};
